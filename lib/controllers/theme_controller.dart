abb23d76fb8b20f10696cdc44c32f990e9615c74
絵夢羽さ沂 <mausu2526@gmail.com>
2025-07-17 23:21:08 +0800

    Feat: Save Theme

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends GetxController {
+  static const _prefKey = 'theme_mode';
+
   final themeMode = ThemeMode.light.obs;
 
-  void setThemeMode(ThemeMode mode) {
+  /// 初始化時讀取儲存的主題設定
+  @override
+  void onInit() {
+    super.onInit();
+    _loadThemeFromPrefs();
+  }
+
+  /// 設定主題模式，並寫入儲存
+  void setThemeMode(ThemeMode mode) async {
     if (mode != themeMode.value) {
       themeMode.value = mode;
+      update();
+      await _saveThemeToPrefs(mode);
+    }
+  }
+
+  /// 儲存主題到 SharedPreferences
+  Future<void> _saveThemeToPrefs(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_prefKey, mode.name); // 用 mode.name 儲存 'light' / 'dark' / 'system'
+  }
+
+  /// 從 SharedPreferences 載入主題
+  Future<void> _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final storedMode = prefs.getString(_prefKey);
+    if (storedMode != null) {
+      switch (storedMode) {
+        case 'light':
+          themeMode.value = ThemeMode.light;
+          break;
+        case 'dark':
+          themeMode.value = ThemeMode.dark;
+          break;
+        case 'system':
+          themeMode.value = ThemeMode.system;
+          break;
+      }
       update();
     }
   }
