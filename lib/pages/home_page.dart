1a930d2c65246cd067e0b510bc9eac493bc74234
絵夢羽さ沂 <mausu2526@gmail.com>
2025-07-20 12:39:12 +0800

    Feat: Modified

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,8 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rabbit_kingdom/controllers/theme_controller.dart';
 import 'package:rabbit_kingdom/controllers/user_controller.dart';
 import 'package:rabbit_kingdom/helpers/app_colors.dart';
-import 'package:rabbit_kingdom/widgets/r_blurred_overlay.dart';
 import 'package:rabbit_kingdom/widgets/r_button.dart';
 
 class HomePage extends StatelessWidget {
@@ -76,7 +73,7 @@ class _HeaderText extends StatelessWidget {
                     ..style = PaintingStyle.stroke
                     ..strokeWidth = fontSize / 26 * 4
                     ..color = themeController.brightness == Brightness.light ?
-                        Color(0xffe6394d)
+                        Color(0xffe6723d)
                       : AppColors.surfaceContainerHigh,
                 ),
               ),
@@ -88,7 +85,7 @@ class _HeaderText extends StatelessWidget {
                   decoration: TextDecoration.none,
                   fontFamily: 'JFHuninn',
                   color: themeController.brightness == Brightness.light ?
-                    Color(0xffffc1d5)
+                    Color(0xfff2e4df)
                   : AppColors.onSurface,
                 ),
               ),
@@ -126,17 +123,8 @@ class _KingdomView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<ThemeController>(
-      builder: (controller) {
-        final themeController = Get.find<ThemeController>();
-        final mode = themeController.themeMode.value;
-        final brightness = () {
-          if (mode == ThemeMode.system) {
-            return MediaQuery.platformBrightnessOf(Get.context!);
-          }
-          return mode == ThemeMode.dark ? Brightness.dark : Brightness.light;
-        }();
-        final phase = brightness == Brightness.light ? "day" : "night";
-
+      builder: (themeController) {
+        final phase = themeController.brightness == Brightness.light ? "day" : "night";
         final screenWidth = MediaQuery.of(context).size.width;
 
         // 根據原始背景比例換算高度
