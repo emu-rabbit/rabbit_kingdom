cd1914077fd9b48616acb881d40a4e7b8a9da74c
絵夢羽さ沂 <mausu2526@gmail.com>
2025-08-05 14:59:28 +0800

    Refactor popup

--- lib/widgets/r_popup.dart
+++ lib/widgets/r_popup.dart
@@ -16,54 +16,65 @@ class RPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: Color(0xaa000000),
-      child: SafeArea(
-        child: Center(
-          child: IntrinsicHeight(
-            child: Container(
-              width: width ?? vw(70) * deviceFactor(),
-              decoration: BoxDecoration(
-                color: AppColors.secondary.withAlpha(220),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                border: Border.all(
-                  color: AppColors.onSecondary,
-                  width: 2
-                )
-              ),
-              child: Stack(
-                children: [
-                  closable ? Align(
-                    alignment: Alignment.topRight,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                      child: RIconButton(
-                          icon: FontAwesomeIcons.xmark,
-                          color: AppColors.onSecondary,
-                          onPress: (){ Get.back(); }
-                      ),
+    return PopScope(
+      canPop: closable,
+      child: GestureDetector(
+        onTap: (){
+          if (closable) Get.back();
+        },
+        child: ColoredBox(
+          color: Color(0xaa000000),
+          child: SafeArea(
+            child: Center(
+              child: IntrinsicHeight(
+                child: GestureDetector(
+                  onTap: (){},
+                  child: Container(
+                    width: width ?? vw(70) * deviceFactor(),
+                    decoration: BoxDecoration(
+                        color: AppColors.secondary.withAlpha(220),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        border: Border.all(
+                            color: AppColors.onSecondary,
+                            width: 2
+                        )
                     ),
-                  ): SizedBox.shrink(),
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: RText.titleLarge(title ?? "", color: AppColors.onSecondary,)
+                    child: Stack(
+                      children: [
+                        closable ? Align(
+                          alignment: Alignment.topRight,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                            child: RIconButton(
+                                icon: FontAwesomeIcons.xmark,
+                                color: AppColors.onSecondary,
+                                onPress: (){ Get.back(); }
+                            ),
+                          ),
+                        ): SizedBox.shrink(),
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: RText.titleLarge(title ?? "", color: AppColors.onSecondary,)
+                          ),
+                        ),
+                        Align(
+                            alignment: Alignment.topCenter,
+                            child: Padding(
+                              padding: EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
+                              child: child,
+                            )
+                        )
+                      ],
                     ),
                   ),
-                  Align(
-                      alignment: Alignment.topCenter,
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
-                        child: child,
-                      )
-                  )
-                ],
-              ),
-            )
+                )
+              )
+            ),
           )
-        )
-      ),
+        ),
+      )
     );
   }
 
